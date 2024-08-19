4c114f1c6a640fdc2682d23a566b508c92cb01c4
Mandresy <christianmandresy@gmail.com>
2024-08-19 15:08:54 +0300

    Updated config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const path = require("path");
 
 module.exports = function override(config, env) {
   config.resolve.fallback = {
@@ -9,6 +10,11 @@ module.exports = function override(config, env) {
   };
 
   config.plugins.push(new NodePolyfillPlugin());
+
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'console-browserify': path.resolve(__dirname, 'node_modules/console-browserify'),
+  };
 
   return config;
 };
