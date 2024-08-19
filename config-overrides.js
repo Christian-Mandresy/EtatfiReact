5969ba4c5937eed31725efca6d2012878304fb86
Mandresy <christianmandresy@gmail.com>
2024-08-19 21:29:28 +0300

    resolving console-browserify

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const { override, addWebpackAlias } = require('customize-cra');
 const path = require("path");
 
 module.exports = function override(config, env) {
@@ -9,12 +10,12 @@ module.exports = function override(config, env) {
     crypto: require.resolve("crypto-browserify"),
   };
 
-  config.plugins.push(new NodePolyfillPlugin());
-
-  config.resolve.alias = {
-    ...config.resolve.alias,
+  addWebpackAlias({
     'console-browserify': path.resolve(__dirname, 'node_modules/console-browserify'),
-  };
+  })
+
+  // Ajout de NodePolyfillPlugin pour les polyfills des modules Node.js
+  config.plugins.push(new NodePolyfillPlugin());
 
   return config;
 };
